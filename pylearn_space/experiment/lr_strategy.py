b523775825cb20ffd91ae2468212bd3eb8160a29
CynicalRose <294366031@qq.com>
2023-01-21 10:25:34 +0800

    学习率绘制

--- pylearn_space/experiment/lr_strategy.py
+++ pylearn_space/experiment/lr_strategy.py
@@ -1,12 +1,15 @@
 # 学习率绘制
 from matplotlib import pyplot as plt
 
+plt.rcParams['font.sans-serif'] = ['SimHei']  #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  #用来正常显示负号
+
 max_epoch = 120  # 最大迭代数
 init_lr = 0.00035  # 3.5 × 10^-4
 lr = init_lr
 warm_up_step = 10  # 前n个epoch启用warm-up策略
 
-x_epoch = list(range(1, max_epoch+1))  # 横坐标列表
+x_epoch = list(range(1, max_epoch + 1))  # 横坐标列表
 lr_list_1 = []  # 记录学习率
 lr_list_2 = []  # 记录学习率
 lr_list_3 = []  # 记录学习率
@@ -21,7 +24,7 @@ def warm_up_rate_o(epoch):
 
         return init_lr
     else:
-        lr = lr ** 1.01
+        lr = lr**1.01
         return lr
 
 
@@ -38,20 +41,18 @@ def warm_up_rate_t(epoch):
         return init_lr * 0.01
 
 
-for train_steps in range(1, max_epoch+1):
+for train_steps in range(1, max_epoch + 1):
     lr_list_1.append(warm_up_rate_o(train_steps))
     lr_list_2.append(warm_up_rate_t(train_steps))
     lr_list_3.append(init_lr)
 
 plt.figure('lr')
-plt.title('lr-strategy')
-plt.xlabel('epoch')
-plt.ylabel('learning rate')
+plt.title('学习率策略')
+plt.xlabel('学习轮次')
+plt.ylabel('学习率')
 plt.ylim()
-plt.plot(x_epoch, lr_list_1, label='ours')
-plt.plot(x_epoch, lr_list_2, '--', color='#f58220',
-         label='tradition', linewidth=1.2)
-plt.plot(x_epoch, lr_list_3, '-.', color='#77ac98',
-         label='no trick', linewidth=1)
+plt.plot(x_epoch, lr_list_1, label='本文策略')
+plt.plot(x_epoch, lr_list_2, '--', color='#f58220', label='传统策略', linewidth=1.2)
+plt.plot(x_epoch, lr_list_3, '-.', color='#77ac98', label='无技巧', linewidth=1)
 plt.legend()
 plt.show()
